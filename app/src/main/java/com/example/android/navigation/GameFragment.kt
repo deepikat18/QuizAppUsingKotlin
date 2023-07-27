14d95437a251b5001de7db3edeb0102f21f3221e
Deepika <deepikathiruvallamgadu@gmail.com>
2023-07-27 08:09:34 +0530

    commited

---
+++ app/src/main/java/com/example/android/navigation/GameFragment.kt
@@ -0,0 +1,128 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.navigation
+
+import androidx.databinding.DataBindingUtil
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.example.android.navigation.databinding.FragmentGameBinding
+
+class GameFragment : Fragment() {
+    data class Question(
+            val text: String,
+            val answers: List<String>)
+
+    // The first answer is the correct one.  We randomize the answers before showing the text.
+    // All questions must have four answers.  We'd want these to contain references to string
+    // resources so we could internationalize. (or better yet, not define the questions in code...)
+    private val questions: MutableList<Question> = mutableListOf(
+            Question(text = "What is the capital city of France?",
+                    answers = listOf(" Paris", "Rome", "London", "Berlin")),
+            Question(text = "Who wrote the play \"Romeo and Juliet\"?",
+                    answers = listOf(" William Shakespeare", "Oscar Wilde", "Jane Austen", "Mark Twain")),
+            Question(text = "What is the largest planet in our solar system?",
+                    answers = listOf(" Jupiter", "Saturn","Venus", "Mars")),
+            Question(text = "Which ocean is the largest on Earth?",
+                    answers = listOf("Pacific Ocean", "Atlantic Ocean", "Indian Ocean", "Arctic Ocean")),
+            Question(text = "What is the currency of Japan?",
+                    answers = listOf("Yen", "Euro", "Doller", "Pound")),
+            Question(text = "Which famous artist painted the Mona Lisa?",
+                    answers = listOf("Leonardo da Vinci", "Michelangelo", "Vincent van Gogh", " Pablo Picasso")),
+            Question(text = "What is the tallest mountain in the world?",
+                    answers = listOf("Mount Everest", "Mount Kilimanjaro", "Mount McKinley (Denali)", " Mount Fuji")),
+            Question(text = "In which country did the Olympic Games originate?",
+                    answers = listOf("Greece", "Egypt", " Italy", " China")),
+            Question(text = "What is the chemical symbol for water?",
+                    answers = listOf(" H2O", "CO2", " O2", "NaCl")),
+            Question(text = "What is the largest organ in the human body?",
+                    answers = listOf("Skin", "Liver", " Brain", "Heart"))
+    )
+
+    lateinit var currentQuestion: Question
+    lateinit var answers: MutableList<String>
+    private var questionIndex = 0
+    private val numQuestions = Math.min((questions.size + 1) / 2, 3)
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        // Inflate the layout for this fragment
+        val binding = DataBindingUtil.inflate<FragmentGameBinding>(
+                inflater, R.layout.fragment_game, container, false)
+
+        // Shuffles the questions and sets the question index to the first question.
+        randomizeQuestions()
+
+        // Bind this fragment class to the layout
+        binding.game = this
+
+        // Set the onClickListener for the submitButton
+        binding.submitButton.setOnClickListener { view: View ->
+            val checkedId = binding.questionRadioGroup.checkedRadioButtonId
+            // Do nothing if nothing is checked (id == -1)
+            if (-1 != checkedId) {
+                var answerIndex = 0
+                when (checkedId) {
+                    R.id.secondAnswerRadioButton -> answerIndex = 1
+                    R.id.thirdAnswerRadioButton -> answerIndex = 2
+                    R.id.fourthAnswerRadioButton -> answerIndex = 3
+                }
+                // The first answer in the original question is always the correct one, so if our
+                // answer matches, we have the correct answer.
+                if (answers[answerIndex] == currentQuestion.answers[0]) {
+                    questionIndex++
+                    // Advance to the next question
+                    if (questionIndex < numQuestions) {
+                        currentQuestion = questions[questionIndex]
+                        setQuestion()
+                        binding.invalidateAll()
+                    } else {
+                        // We've won!  Navigate to the gameWonFragment.
+                        view.findNavController().navigate(GameFragmentDirections.actionGameFragmentToGameWonFragment(numQuestions,questionIndex))
+                    }
+                } else {
+                    // Game over! A wrong answer sends us to the gameOverFragment.
+                    view.findNavController().navigate(GameFragmentDirections.actionGameFragmentToGameOverFragment())
+                }
+            }
+        }
+        return binding.root
+    }
+
+    // randomize the questions and set the first question
+    private fun randomizeQuestions() {
+        questions.shuffle()
+        questionIndex = 0
+        setQuestion()
+    }
+
+    // Sets the question and randomizes the answers.  This only changes the data, not the UI.
+    // Calling invalidateAll on the FragmentGameBinding updates the data.
+    private fun setQuestion() {
+        currentQuestion = questions[questionIndex]
+        // randomize the answers into a copy of the array
+        answers = currentQuestion.answers.toMutableList()
+        // and shuffle them
+        answers.shuffle()
+        (activity as AppCompatActivity).supportActionBar?.title = getString(R.string.title_android_trivia_question, questionIndex + 1, numQuestions)
+    }
+}
